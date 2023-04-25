8d81f54f8f27a39c60c330be18f97c16b06ac0e4
rakesh4118 <rakii.s1>
2023-04-26 01:14:56 +0530

    queryselect

---
+++ dom6/dom3query.js
@@ -0,0 +1,28 @@
+/*console.dir(document);
+console.log(document.domain);
+
+
+
+
+
+
+// QUERY SELECTOR
+
+/*var header = document.querySelector("#main-header");
+header.style.borderBottom = "solid 4px #ccc";
+
+var seconditem = document.querySelector(".list-group-item:nth-child(2)");
+seconditem.style.backgroundColor = "green"
+
+var thirditem = document.querySelector(".list-group-item:nth-child(3)");
+thirditem.style.display ="none";*/
+
+// QUERY SELECTORALL
+var items = document.querySelectorAll(".list-group-item");
+items[1].style.color = "green";
+
+var odd = document.querySelectorAll("li:nth-child(odd)");
+
+for(var i = 0;i < odd.length;i++){
+    odd[i].style.backgroundColor = "green"
+}
